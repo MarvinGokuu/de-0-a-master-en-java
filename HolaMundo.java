6075908ab8701433da6699ffb2392e62dc67dbb0
MARVIN DEV sv <thecanales23@gmail.com>
2025-10-18 17:37:14 -0600

    update rutade0.java

---
+++ HolaMundo.java
@@ -0,0 +1,32 @@
+
+
+public class HolaMundo {
+
+    public static void mostrarSaludo() throws InterruptedException {
+        String saludar = "Hola Mundo desde Java!";
+
+        System.out.println("\nSaludo animado y evolutivo:\n");
+
+        escribirConAnimacion(saludar, 100);
+
+        for (int i = 0; i < 20; i++) {
+            System.out.print("\r" + " ".repeat(i) + saludar);
+            Thread.sleep(150);
+        }
+        System.out.println();
+
+        for (char c : saludar.toCharArray()) {
+            System.out.print("*" + c);
+            Thread.sleep(80);
+        }
+        System.out.println("\n");
+    }
+
+    private static void escribirConAnimacion(String texto, int delayMillis) throws InterruptedException {
+        for (char c : texto.toCharArray()) {
+            System.out.print(c);
+            Thread.sleep(delayMillis);
+        }
+        System.out.println();
+    }
+}
---
+++ io/github/theca/comandos/Main.java
@@ -0,0 +1,27 @@
+package io.github.theca.comandos;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("=== CONSOLA INTERACTIVA ===");
+        System.out.println("Comandos disponibles: saludo, salir\n");
+
+        while (true) {
+            System.out.print("> ");
+            String comando = scanner.nextLine().trim().toLowerCase();
+
+            switch (comando) {
+                case "saludo":
+                    SaludoCommand.ejecutar();
+                    break;
+                case "salir":
+                    System.out.println("👋 Saliendo...");
+                    return;
+                default:
+                    System.out.println("Comando no reconocido.");
+            }
+        }
+    }
+}
